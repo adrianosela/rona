6f86372a59183f0292f460cabe1425c5eb30863c
Adriano <adriano.selaviles@gmail.com>
2020-03-23 20:21:07 -0700

    latest

--- countryplot.py
+++ countryplot.py
@@ -1,3 +1,5 @@
+#!/usr/bin/env python
+
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
@@ -5,28 +7,30 @@ import qeds
 from sklearn import (
     linear_model, metrics, neural_network, pipeline, model_selection, preprocessing
 )
+from functools import reduce
+
+DATASET_URLS = {
+    'confirmed':'https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_19-covid-Confirmed.csv',
+    'recoveries': 'https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_19-covid-Recovered.csv',
+    'deaths': 'https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_19-covid-Deaths.csv'
+}
 
 # activate plot theme
 qeds.themes.mpl_style();
 
-#!/usr/bin/env python
-
-import requests
-
-CONFIRMED_URL = 'https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_19-covid-Confirmed.csv'
-RECOVERIES_URL = 'https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_19-covid-Recovered.csv'
-DEATHS_URL = 'https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_19-covid-Deaths.csv'
-
-DATASET_URLS = {
-    "confirmed": CONFIRMED_URL,
-    "recoveries": RECOVERIES_URL,
-    "deaths": DEATHS_URL
-}
-
+# load CSV data to a dict of dataset name to pandas dataframe
 DATASETS = dict()
-
 for dset in DATASET_URLS:
     DATASETS[dset] = pd.read_csv(DATASET_URLS[dset])
 
-for dset in DATASET_URLS:
-    print(DATASETS[dset].head())
+# data comes in "wide" format, reshape to “long” format with 
+# one country-date combination per row and counts in a single column
+ids = ["Province/State","Country/Region", "Lat","Long"]
+for dset in DATASETS:
+    DATASETS[dset] = DATASETS[dset].melt(id_vars=ids, var_name="Date", value_name=dset)
+
+# merge all the dataframes on date
+covid = reduce(lambda x, y: pd.merge(x, y, on="Date"), list(DATASETS.values()))
+covid["Date"] = pd.to_datetime(covid["Date"])
+covid["Province/State"]=covid["Province/State"].fillna("")
+print(covid.groupby("Country/Region").get_group("Canada").groupby("Province/State").tail(1))
