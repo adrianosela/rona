df064f0c359026cfdee25a82158cd7d73ffc4c0c
Adriano <adriano.selaviles@gmail.com>
2020-03-24 11:51:01 -0700

    latest

--- countryplot.py
+++ countryplot.py
@@ -3,36 +3,29 @@
 import datasets
 import pandas as pd
 import matplotlib.pyplot as plt
-from functools import reduce
-
 import qeds
+import sys
 
 # activate plot theme
 qeds.themes.mpl_style()
 
-# download latest version of dataset
+# download latest version of data
 dsets = datasets.load()
+covid = datasets.combine(dsets)
 
-# data comes in "wide" format, reshape to “long” format with 
-# one country-date combination per row and counts in a single column
-ids = ["Province/State", "Country/Region", "Lat", "Long"]
-for dset in dsets:
-    dsets[dset] = dsets[dset].melt(id_vars=ids, var_name="Date", value_name=dset)
+def plot_country(country):
+    try:
+        by_country = covid.groupby(datasets.COUNTRY_ID).get_group(country)
+    except:
+        print(f'no data for country: "{country}"')
+        sys.exit(1)
 
-# merge all the dataframes on date
-covid = reduce(lambda x, y: pd.merge(x, y, on=ids.append("Date")), list(dsets.values()))
+    # plot confirmed cases data for country per province/state
+    fig, ax = plt.subplots(figsize=(10,6))
+    for prov, df in by_country.reset_index().groupby("Province/State"):
+        df.plot(x="Date", y=datasets.CONFIRMED, ax=ax, label=prov)
+    ax.set_title("Cases by Province")
+    plt.show()
 
-# format date and populate empty cells
-covid["Date"] = pd.to_datetime(covid["Date"])
-covid["Province/State"] = covid["Province/State"].fillna("")
-
-# dataframe for specific country
-country = covid.groupby("Country/Region").get_group("Canada") # FIXME: make variable
-
-# plot confirmed cases data for country per province/state
-fig, ax = plt.subplots(figsize=(10,6))
-for prov, df in country.reset_index().groupby("Province/State"):
-    df.plot(x="Date", y=datasets.CONFIRMED, ax=ax, label=prov)
-ax.set_title("Cases by Province")
-
-plt.show()
+if __name__ == "__main__":
+    plot_country(sys.argv[1])
--- datasets.py
+++ datasets.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 
 import pandas as pd
-import requests
+from functools import reduce
 
 CONFIRMED = 'confirmed'
 RECOVERIES = 'recoveries'
@@ -13,15 +13,38 @@ DATASET_URLS = {
     DEATHS: 'https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_19-covid-Deaths.csv'
 }
 
+PROVINCE_ID = 'Province/State'
+COUNTRY_ID = 'Country/Region'
+LATITUDE_ID = 'Lat'
+LONGITUDE_ID = 'Long'
+DATE_ID = 'Date'
+
+COLUMN_IDS = [
+    PROVINCE_ID,
+    COUNTRY_ID,
+    LATITUDE_ID,
+    LONGITUDE_ID
+]
+
+# returns a dict of dataset name to pandas dataframe
 def load():
     dsets = dict()
     for dset in DATASET_URLS:
         dsets[dset] = pd.read_csv(DATASET_URLS[dset])
     return dsets
 
-def download():
-    for dset in DATASET_URLS:
-        resp = requests.get(DATASET_URLS[dset])
-        with open("%s.csv" % dset, 'w') as f:
-            f.write(resp.text)
-        f.close()
+# returns a pandas dataframe of all datasets combined
+def combine(dsets):
+    # data comes in "wide" format, reshape to “long” format with
+    # one country-date combination per row and counts in a single column
+    for dset in dsets:
+        dsets[dset] = dsets[dset].melt(id_vars=COLUMN_IDS, var_name=DATE_ID, value_name=dset)
+    # merge all the dataframes on date
+    combined = reduce(lambda x, y: pd.merge(x, y, on=COLUMN_IDS.append(DATE_ID)), list(dsets.values()))
+    # format date and populate empty cells
+    combined[DATE_ID] = pd.to_datetime(combined[DATE_ID])
+    combined[PROVINCE_ID] = combined[PROVINCE_ID].fillna("")
+    return combined
+
+if __name__ == "__main__":
+    combine(load()).to_csv("covid.csv", index=False)
